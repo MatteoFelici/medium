bf40c21cb6cbd3fe30051b872f30b7b1e5e8cb78
Matteo Felici <matteo.felici@facile.it>
2020-10-06 08:57:34 +0200

    [ai/tuning][WIP] Added source code

---
+++ ai-platform-tuning/src/tune.py
@@ -0,0 +1,141 @@
+import argparse
+import os
+import joblib
+import subprocess
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, \
+    f1_score
+import pandas as pd
+
+
+STORAGE_BUCKET = 'bank-marketing-model'
+DATA_PATH = 'bank-additional-full.csv'
+LOCAL_PATH = '/tmp'
+PROJECT_ID = 'medium-articles'
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--storage-path', type=str, required=True,
+                        help='Google Storage path where to store training '
+                             'artifacts (string, required)')
+    parser.add_argument('--test-size', type=float, default=0.2,
+                        help='Percentage of data to use as test set (float, '
+                             'default 0.2)')
+    parser.add_argument('--n-estimators', type=int, default=100,
+                        help='Number of trees in Random Forest model '
+                             '(integer, default 100)')
+    parser.add_argument('--max-depth', type=int, default=10,
+                        help='Maximum depth of each tree in Random Forest model'
+                             ' (integer, default 10)')
+    parser.add_argument('--min-samples_split', type=int, default=0.05,
+                        help='Minimum number of samples (as fraction of total) '
+                             'to split a node of a tree (float, default 0.05)')
+    parser.add_argument('--max-features', type=float, default=None,
+                        help='Number of features to use (as fraction of total) '
+                             'for each tree in Random Forest model (float, '
+                             'default square root of number of columns)')
+    parser.add_argument('--max-samples', type=int, default=0.5,
+                        help='Number of samples to use (as fraction of total) '
+                             'for each tree in Random Forest model (float, '
+                             'default 0.5)')
+    parser.add_argument('--n-jobs', type=int, default=1,
+                        help='Number of parallel jobs to run (int, default 1)')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Download dataset
+    subprocess.call([
+        'gsutil', 'cp',
+        # Storage path
+        os.path.join('gs://', STORAGE_BUCKET, DATA_PATH),
+        # Local path
+        os.path.join(LOCAL_PATH, 'dataset.csv')
+    ])
+    
+    # Read data with pandas - separator is ';'
+    df = pd.read_csv(os.path.join(LOCAL_PATH, 'dataset.csv'), sep=';')
+    
+    # Split data between train and test
+    train, test = train_test_split(df, test_size=args.test_size)
+
+    y_train = train['y']
+    train = train.drop('y', 1)
+    y_test = test['y']
+    test = test.drop('y', 1)
+
+    # Create a scikit-learn pipeline with preprocessing steps + model
+
+    # First, define numeric and categorical features to use
+    num_features = ['age', 'duration', 'campaign', 'pdays', 'previous',
+                    'emp.var.rate', 'cons.price.idx', 'cons.conf.idx',
+                    'euribor3m', 'nr.employed']
+    cat_features = ['job', 'marital', 'education', 'default', 'housing', 'loan',
+                    'contact', 'month', 'day_of_week', 'poutcome']
+
+    pipeline = Pipeline([
+        # The ColumnTransformer divide the preprocessing process between
+        # categorical and numerical data
+        ('data_prep',
+         ColumnTransformer([
+            ('num_prep', StandardScaler(), num_features),
+            ('cat_prep', OneHotEncoder(), cat_features)
+         ])),
+        # ML model
+        ('model',
+         RandomForestClassifier(
+             random_state=1123,
+             n_jobs=args.n_jobs,
+             n_estimators=args.n_estimators,
+             max_depth=args.max_depth,
+             max_features=args.max_features if args.max_features is not
+                                                  None else 'sqrt',
+             min_samples_split=args.min_samples_split,
+             class_weight='balanced',
+             max_samples=args.max_samples
+         ))
+    ])
+
+    # Train the model
+    pipeline.fit(train, y_train)
+
+    # Save model
+    joblib.dump(pipeline, os.path.join(LOCAL_PATH, 'model.joblib'))
+
+    # Get predictions
+    pred_train = pipeline.predict(train)
+    pred_test = pipeline.predict(test)
+
+    # Calculate a bunch of performance metrics
+    results = pd.DataFrame(
+        {'accuracy': [accuracy_score(y_train, pred_train),
+                      accuracy_score(y_test, pred_test)],
+         'precision': [precision_score(y_train, pred_train, pos_label='yes'),
+                       precision_score(y_test, pred_test, pos_label='yes')],
+         'recall': [recall_score(y_train, pred_train, pos_label='yes'),
+                    recall_score(y_test, pred_test, pos_label='yes')],
+         'f1': [f1_score(y_train, pred_train, pos_label='yes'),
+                f1_score(y_test, pred_test, pos_label='yes')]},
+        index=['train', 'test']
+    )
+    results.to_csv(os.path.join(LOCAL_PATH, 'results.csv'))
+
+    # Upload model and results Dataframe to Storage
+    subprocess.call([
+        'gsutil', 'cp',
+        # Local path of the model
+        os.path.join(LOCAL_PATH, 'model.joblib'),
+        os.path.join(args.storage_path, 'model.joblib')
+    ])
+    subprocess.call([
+        'gsutil', 'cp',
+        # Local path of results
+        os.path.join(LOCAL_PATH, 'results.csv'),
+        os.path.join(args.storage_path, 'results.csv')
+    ])
--- ai-platform-tuning/tuning_AI.md
+++ ai-platform-tuning/tuning_AI.md
@@ -1,5 +1,5 @@
 # Google ML tutorials - Tuning a model with Bayesian Optimization on AI Platform
-On this article of the [Google ML tutorials](https://towardsdatascience.com/tagged/google-ml-tutorials)
+On this article of the [Google ML tutorials][ML tutorials series]
 series, we will talk about how to use the [AI Platform](https://cloud.google.com/ai-platform)
 built-in tool to tune the hyperparameters of your Machine Learning model! We
 will use a method called Bayesian Optimization to navigate the hyperparameters 
@@ -25,3 +25,34 @@ hyperparameters space. It then explores this distribution trying different
 sets of hyperparameters. At each trial, it gains more information (in Bayes 
 fashion) about the real distribution of the objective function, so it can move 
 to a more "promising" subset of the domain space.
+
+For this specific reason, keep in mind that we cannot fully parallelize the process of the Bayesian Optimization (as opposed to Grid and Random Search), since each iteration learns from the previous one.
+
+Now let's train some models! For the tutorial, we will follow the same steps of the [training tutorial][Training article]:
+- store the data on Google Storage
+- write a Python application to train the model
+- launch a training job on AI Platform
+
+The big differences are on the Python application itself: we need to add a
+framework to chain the model's performance results to the Bayesian Optimization. 
+This framework is called [Hypertune](https://github.com/GoogleCloudPlatform/cloudml-hypertune): 
+you can install it simply with `pip install cloudml-hypertune`.
+
+## Changing the Python application
+First thing, we need to perform **cross-validation** on training data. For those non familiar with the cross-validation approach, it goes like this:
+1. you divide your data into *n* splits
+2. choose one split as *validation*
+3. concatenate the remaining *n-1* splits and train the model on this new dataset
+4. calculate the performance on the hold-out split
+5. repeat 2-4 on each split
+
+This method is suitable if you want to robustly assess one single model, because you train and validate it on *n* potentially different scenarios. 
+
+
+ 
+
+ 
+
+
+[ML tutorials series]: https://towardsdatascience.com/tagged/google-ml-tutorials
+[Training article]: https://towardsdatascience.com/training-a-model-on-google-ai-platform-84ceff87b5f3
